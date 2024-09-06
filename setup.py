bdb40457ce6f7f31b1574b5754473d0717862d02
EricPossato <ericandreilimapossato@gmail.com>
2024-09-06 07:29:01 -0300

    Update to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='dev_aberto_ericalp',
-      version='0.1',
+      version='0.2',
       packages=['dev_aberto'],
       scripts=['scripts/hello.py'],
       python_requires='>=3.12',
