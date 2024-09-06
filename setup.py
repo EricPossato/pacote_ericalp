5eb32c79c636678c6ae49a869b67877b45d89ef1
EricPossato <ericandreilimapossato@gmail.com>
2024-09-06 07:07:08 -0300

    setup + requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3
 from setuptools import setup
 
 setup(name='dev_aberto_ericalp',
       version='0.1',
-      packages=['dev_aberto']
+      packages=['dev_aberto'],
+      scripts=['scripts/hello.py'],
+      python_requires='>=3.12',
+      install_requires=['requests']
       )
