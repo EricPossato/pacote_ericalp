c9a2e9ad6b0f3ee9e697a3fb1835c938110c57a0
EricPossato <ericandreilimapossato@gmail.com>
2024-09-23 10:06:08 -0300

    English translation

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 from dev_aberto import hello
+import babel.dates as dates
+from datetime import datetime
+from datetime import date
+
+import gettext
+gettext.bindtextdomain('cli', 'locale')
+gettext.textdomain('cli')
+_ = gettext.gettext
 
 if __name__ == '__main__':
-    date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    try:
+        data, name = hello()
+        data = datetime.strptime(data, '%Y-%m-%dT%H:%M:%S%z')
+    except:
+        data, name = date.today(), '???'
+    
+    data = dates.format_datetime(data)
+    print(_('Último commit feito em: {} por {}').format(data, name))
